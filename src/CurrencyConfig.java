23aa043d57987ec19bfdd47d246f99bcdfa74327
Araken lobao <arakenlobao20@gmail.com>
2025-07-20 00:33:03 -0300

    Projeto conversor de moedas

---
+++ src/CurrencyConfig.java
@@ -0,0 +1,11 @@
+public class CurrencyConfig {
+    private String defaultBaseCurrency = "USD";
+
+    public String getDefaultBaseCurrency() {
+        return defaultBaseCurrency;
+    }
+
+    public void setDefaultBaseCurrency(String defaultBaseCurrency) {
+        this.defaultBaseCurrency = defaultBaseCurrency;
+    }
+}
